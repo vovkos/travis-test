c89e273f706d92971dc5747ed31d6175c7aa376d
Vladimir Gladkov <vovkos@gmail.com>
2017-06-08 16:03:08 +0800

    test deploy to jancy.org

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,22 @@
 echo Deploying...
-echo ---------------- > out.txt
-echo token: $TOKEN >> out.txt
-echo ---------------- >> out.txt
 
-cat out.txt
+openssl \
+	aes-256-cbc \
+	-K $encrypted_96b78b9d55e6_key \
+	-iv $encrypted_96b78b9d55e6_iv \
+	-in travis_id_rsa.enc \
+	-out travis_id_rsa \
+	-d
+
+chmod 600 travis_id_rsa
+
+sftp \
+	-i travis_id_rsa \
+	-o StrictHostKeyChecking=no \
+	travis@jancy.org \
+	<< ===
+
+	cd incoming
+	put artifact.txt
+	bye
+===
